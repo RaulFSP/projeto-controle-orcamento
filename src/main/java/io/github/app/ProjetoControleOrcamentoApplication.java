ae9144dc2fc0355943a87173b609e5a04d2f14b1
raul <raulfs@protonmail.com>
2025-09-19 22:28:35 -0400

    criando o projeto

---
+++ src/main/java/io/github/app/ProjetoControleOrcamentoApplication.java
@@ -0,0 +1,13 @@
+package io.github.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoControleOrcamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoControleOrcamentoApplication.class, args);
+	}
+
+}
